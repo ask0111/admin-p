d1f155c0c643ede0cc38921ed094affcc6c8a70a
Hariom Yadav <hariomask0111@gmail.com>
2023-08-04 11:16:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const data= [
+    {
+        image: 'https://i.pinimg.com/736x/24/f8/1a/24f81abe60db774def193f9639853b6c--victorian-dresses-victorian-fashion.jpg',
+        title: 'Popular admin template you can use for your business.',
+        discription: 'It is a long established fact that a reader will be distract by the readable content.',
+        category: 'Fashion',
+        name: 'Fitbit Incorporation',
+        job: 'San Dlego, California'
+    },
+    {
+        image: 'https://content.jdmagicbox.com/comp/chennai/y3/044pxx44.xx44.101021162125.a7y3/catalogue/indian-school-of-business-management-and-administration-nungambakkam-chennai-fashion-designing-institutes-tnf5xozc1o.jpg',
+        title: 'Popular admin template you can use for your business.',
+        discription: 'It is a long established fact that a reader will be distract by the readable content.',
+        category: 'Fashion',
+        name: 'Fitbit Incorporation',
+        job: 'San Dlego, California'
+    },
+    {
+        title: 'Popular admin template you can use for your business.',
+        discription: 'It is a long established fact that a reader will be distract by the readable content.',
+        category: 'Fashion',
+        name: 'Fitbit Incorporation',
+        job: 'San Dlego, California'
+    },
+    {
+        title: 'Popular admin template you can use for your business.',
+        discription: 'It is a long established fact that a reader will be distract by the readable content.',
+        category: 'Fashion',
+        name: 'Fitbit Incorporation',
+        job: 'San Dlego, California'
+    },
+    {
+        title: 'Popular admin template you can use for your business.',
+        discription: 'It is a long established fact that a reader will be distract by the readable content.',
+        category: 'Fashion',
+        name: 'Fitbit Incorporation',
+        job: 'San Dlego, California'
+    },
+    {
+        title: 'Popular admin template you can use for your business.',
+        discription: 'It is a long established fact that a reader will be distract by the readable content.',
+        category: 'Fashion',
+        name: 'Fitbit Incorporation',
+        job: 'San Dlego, California'
+    },
+] 
